97713abc3111af35ca092d8d4279d021722496e5
Joze <joze@vivobook.asus-vivobook>
2023-08-24 23:53:02 +0200

    add

--- add.c
+++ add.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * add - Adds the top two values of a stack_t linked list.
  * @stack: A pointer to the top mode node of a stack_t linked list.
@@ -10,10 +11,9 @@ void add(stack_t **stack, unsigned int line_num)
 {
     if ((*stack)->next == NULL || (*stack)->next->next == NULL)
     {
-        fprintf(stderr, "L%u: can't add, stack too short\n",line_num);
+        fprintf(stderr, "L%u: can't add, stack too short\n", line_num);
         return;
     }
-
 
     (*stack)->next->next->n += (*stack)->next->n;
     pop(stack, line_num);
